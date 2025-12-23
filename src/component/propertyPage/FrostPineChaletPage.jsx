474c88d2ccb0a7a770c30430d00e3ae26ad3e8c5
Deepak Shukla <deepakshukla01012000@gmail.com>
2025-12-23 18:16:05 +0530

    sub hero section

--- src/component/propertyPage/FrostPineChaletPage.jsx
+++ src/component/propertyPage/FrostPineChaletPage.jsx
@@ -117,6 +117,73 @@ const FrostPineChaletPage = () => {
                     </div>
                 </section>
 
+                {/* Property Highlights Section */}
+                <section className="property-highlights" aria-label="Property Highlights">
+                    <div className="property-highlights-container">
+                        <div className="property-highlights-list" role="list">
+                            {/* Speakeasy Lounge */}
+                            <div className="property-highlight-item" role="listitem" itemProp="amenityFeature" itemScope itemType="https://schema.org/LocationFeatureSpecification">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/image/icons/SpeakeasyLounge.svg`}
+                                    alt="Speakeasy Lounge"
+                                    className="property-highlight-icon"
+                                    width="60"
+                                    height="60"
+                                />
+                                <span className="property-highlight-label" itemProp="name">Speakeasy Lounge</span>
+                            </div>
+
+                            {/* BBQ Grill & Fire Pit */}
+                            <div className="property-highlight-item" role="listitem" itemProp="amenityFeature" itemScope itemType="https://schema.org/LocationFeatureSpecification">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/image/icons/bbq.svg`}
+                                    alt="BBQ Grill & Fire Pit"
+                                    className="property-highlight-icon"
+                                    width="60"
+                                    height="60"
+                                />
+                                <span className="property-highlight-label" itemProp="name">BBQ Grill & Fire Pit</span>
+                            </div>
+
+                            {/* Mountain Views */}
+                            <div className="property-highlight-item" role="listitem" itemProp="amenityFeature" itemScope itemType="https://schema.org/LocationFeatureSpecification">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/image/icons/mountain.svg`}
+                                    alt="Mountain Views"
+                                    className="property-highlight-icon"
+                                    width="60"
+                                    height="60"
+                                />
+                                <span className="property-highlight-label" itemProp="name">Mountain Views</span>
+                            </div>
+
+                            {/* Game Room */}
+                            <div className="property-highlight-item" role="listitem" itemProp="amenityFeature" itemScope itemType="https://schema.org/LocationFeatureSpecification">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/image/icons/game.svg`}
+                                    alt="Game Room"
+                                    className="property-highlight-icon"
+                                    width="60"
+                                    height="60"
+                                />
+                                <span className="property-highlight-label" itemProp="name">Game Room</span>
+                            </div>
+
+                            {/* Sunday River Resort */}
+                            <div className="property-highlight-item" role="listitem" itemProp="amenityFeature" itemScope itemType="https://schema.org/LocationFeatureSpecification">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/image/icons/river.svg`}
+                                    alt="Sunday River Resort"
+                                    className="property-highlight-icon"
+                                    width="60"
+                                    height="60"
+                                />
+                                <span className="property-highlight-label" itemProp="name">Sunday River Resort</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
                 {/* More sections will be added here */}
             </main>
         </>
